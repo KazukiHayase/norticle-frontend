ad41a4e952af72c36ce61babae2aba8fd66e0722
KazukiHayase <k.hayase@buysell-technologies.com>
2022-04-01 01:01:32 +0900

    nprogress導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
+import 'nprogress/nprogress.css';
+
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/system';
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Router } from 'next/router';
+import nprogress from 'nprogress';
 import { ReactElement, ReactNode } from 'react';
 
 import { AuthorizedApolloProvider } from '@/providers/authorizedApolloProvider';
 import { AuthnProvider } from '@/services/authn';
 import { theme } from '@/styles/theme';
+
+nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 });
+Router.events.on('routeChangeStart', nprogress.start);
+Router.events.on('routeChangeComplete', nprogress.done);
+Router.events.on('routeChangeError', nprogress.done);
 
 // https://nextjs.org/docs/basic-features/layouts#with-typescript
 type NextPageWithLayout = NextPage & {
