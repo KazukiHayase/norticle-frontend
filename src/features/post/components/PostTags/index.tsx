e8fcf4965744136e5d0cf2bfd0457e6352597321
KazukiHayase <k.hayase@buysell-technologies.com>
2022-04-14 20:14:40 +0900

    タグクリックでタグページに遷移

--- src/features/post/components/PostTags/index.tsx
+++ src/features/post/components/PostTags/index.tsx
@@ -1,6 +1,9 @@
 import { Chip } from '@mui/material';
 import { VFC } from 'react';
 
+import { NextLinkComposed } from '@/components/uiParts/Link';
+import { pagesPath } from '@/lib/$path';
+
 import { PostTagsFragment } from './generated';
 import { Wrapper } from './style';
 
@@ -13,7 +16,14 @@ export const PostTags: VFC<PostTagsProps> = ({ post }) => {
   return (
     <Wrapper>
       {post.taggings.map((tagging) => (
-        <Chip key={tagging.id} size="small" label={tagging.tag.name} />
+        <Chip
+          key={tagging.id}
+          component={NextLinkComposed}
+          to={pagesPath.tags._name(tagging.tag.name).$url()}
+          clickable
+          size="small"
+          label={tagging.tag.name}
+        />
       ))}
     </Wrapper>
   );
