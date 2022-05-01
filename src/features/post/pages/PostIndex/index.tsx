3bfd526b3d99cd991880ed83df175b362089edf3
KazukiHayase <k.hayase@buysell-technologies.com>
2022-05-01 20:46:46 +0900

    新着・トレンドへの導線追加

--- src/features/post/pages/PostIndex/index.tsx
+++ src/features/post/pages/PostIndex/index.tsx
@@ -1,17 +1,22 @@
-import { Container, Grid, Typography } from '@mui/material';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import { blueGrey } from '@mui/material/colors';
 import { filter } from 'graphql-anywhere';
 import { useEffect, useMemo, VFC } from 'react';
 
+import { NextLinkComposed } from '@/components/uiParts/Link';
 import { PostCard } from '@/features/post/components/PostCard';
 import {
   PostCardFragment,
   PostCardFragmentDoc,
 } from '@/features/post/components/PostCard/generated';
+import { pagesPath } from '@/lib/$path';
 import { progress } from '@/services/progress';
 import { Section } from '@/styles';
 
 import { FetchPostsQuery, useFetchPostsQuery } from './generated';
+import { ButtonWrapper } from './style';
 
 const limit = 6;
 
@@ -39,7 +44,7 @@ export const PostIndex: VFC = () => {
           <Typography variant="h1" sx={{ pb: 3 }}>
             💫 新着
           </Typography>
-          <Grid container spacing={{ xs: 4, md: 2 }} sx={{ pb: 8 }}>
+          <Grid container spacing={{ xs: 4, md: 2 }} sx={{ pb: 6 }}>
             {newPosts.map((post) => (
               <Grid key={post.id} item xs={12} md={6}>
                 <PostCard
@@ -51,6 +56,22 @@ export const PostIndex: VFC = () => {
               </Grid>
             ))}
           </Grid>
+          <ButtonWrapper>
+            <Button
+              component={NextLinkComposed}
+              to={pagesPath.posts.$url()}
+              endIcon={
+                <FontAwesomeIcon
+                  icon={faAnglesRight}
+                  style={{ fontSize: 16 }}
+                />
+              }
+              variant="contained"
+              sx={{ fontWeight: 'bold' }}
+            >
+              新着をもっと見る
+            </Button>
+          </ButtonWrapper>
         </Container>
       </Section>
       <Section sx={{ bgcolor: blueGrey[50] }}>
@@ -58,7 +79,7 @@ export const PostIndex: VFC = () => {
           <Typography variant="h1" sx={{ pb: 3 }}>
             🎉 トレンド
           </Typography>
-          <Grid container spacing={{ xs: 4, md: 2 }} sx={{ pb: 8 }}>
+          <Grid container spacing={{ xs: 4, md: 2 }} sx={{ pb: 6 }}>
             {trendPosts.map((post) => (
               <Grid key={post.id} item xs={12} md={6}>
                 <PostCard
@@ -70,6 +91,22 @@ export const PostIndex: VFC = () => {
               </Grid>
             ))}
           </Grid>
+          <ButtonWrapper>
+            <Button
+              component={NextLinkComposed}
+              to={pagesPath.posts.trend.$url()}
+              endIcon={
+                <FontAwesomeIcon
+                  icon={faAnglesRight}
+                  style={{ fontSize: 16 }}
+                />
+              }
+              variant="contained"
+              sx={{ fontWeight: 'bold' }}
+            >
+              トレンドをもっと見る
+            </Button>
+          </ButtonWrapper>
         </Container>
       </Section>
     </>
---
+++ src/features/post/pages/PostIndex/style.ts
@@ -0,0 +1,8 @@
+import { experimental_sx as sx, styled } from '@mui/material';
+
+export const ButtonWrapper = styled('div')(
+  sx({
+    display: 'flex',
+    justifyContent: 'center',
+  }),
+);
